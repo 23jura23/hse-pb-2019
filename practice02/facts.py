db3af723918c206ee5e80ac9f05de933f6359919
Худяков Юрий <catchitoo@mail.ru>
2019-11-15 12:46:23 +0300

    added facts about me

--- practice02/facts.py
+++ practice02/facts.py
@@ -5,8 +5,8 @@ import sys
 
 Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_name', 'facts'])
 people = [
-    Person(github_login='example', first_name='Name', last_name='Surname',
-           facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='23jura23', first_name='y0urass', last_name="or not your's",
+           facts=['I like you <3', 'This statement is false', 'Я люблю прогать на Питоге']),
 ]
 
 answers = {}
